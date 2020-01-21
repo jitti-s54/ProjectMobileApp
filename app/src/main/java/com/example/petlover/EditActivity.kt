217b0caadffe5c7b47f641576ed9903050ed42ca
Krirati <kriratik@kkumail.com>
2020-01-21 17:25:49 +0000

    krirati

---
+++ app/src/main/java/com/example/petlover/EditActivity.kt
@@ -0,0 +1,20 @@
+package com.example.petlover
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit.*
+
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+        cancle.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
+    }
+}
