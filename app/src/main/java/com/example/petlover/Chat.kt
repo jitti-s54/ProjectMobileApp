16ffcd73b1c09d77c5b6a05b5d961bf69a3ce7cf
ThanawatPermngram <thanawatpar@kkumail.com>
2020-01-22 00:23:32 +0700

    ThanawatPermngram

--- app/src/main/java/com/example/petlover/Chat.kt
+++ app/src/main/java/com/example/petlover/Chat.kt
@@ -3,10 +3,10 @@ package com.example.petlover
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Chat : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_searchfilter)
+        setContentView(R.layout.activity_chat)
     }
 }
--- app/src/main/java/com/example/petlover/searchfilter.kt
+++ app/src/main/java/com/example/petlover/searchfilter.kt
@@ -3,7 +3,7 @@ package com.example.petlover
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class searchfilter : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
