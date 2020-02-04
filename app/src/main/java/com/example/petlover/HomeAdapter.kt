e9af4d7297162987a569dca422c95528874ad4ad
Krirati <kriratik@kkumail.com>
2020-02-04 13:30:52 +0700

    th us krirati

--- app/src/main/java/com/example/petlover/HomeAdapter.kt
+++ app/src/main/java/com/example/petlover/HomeAdapter.kt
@@ -3,6 +3,7 @@ package com.example.petlover
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -20,10 +21,14 @@ class HomeAdapter (private val userItems: ArrayList<User>): RecyclerView.Adapter
         val user: User = userItems[position]
         holder.textViewName.text = user.name
         holder.textViewAddress.text = user.address
+        holder.imgIcon.setImageResource(user.icon)
+        holder.imgStatus.setImageResource(user.status)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val textViewName = itemView.findViewById(R.id.text_title) as TextView
+        val textViewName = itemView.findViewById(R.id.text_name) as TextView
         val textViewAddress = itemView.findViewById(R.id.text_description) as TextView
+        val imgIcon = itemView.findViewById(R.id.iconType) as ImageButton
+        val imgStatus = itemView.findViewById(R.id.iconStatus) as ImageButton
     }
 }
--- app/src/main/java/com/example/petlover/User.kt
+++ app/src/main/java/com/example/petlover/User.kt
@@ -1,3 +1,3 @@
 package com.example.petlover
 
-data class User(val name: String,val address: String)
+data class User(val name: String,val address: String, val icon: Int, val status: Int)
