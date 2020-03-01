5094258b92f1e3d99e0f0e1fadc1d22a9be43feb
ThanawatPermngram <thanawatpar@kkumail.com>
2020-03-01 13:23:20 +0700

    Register completed with some bug

--- app/src/main/java/com/example/petlover/LoginActivity.kt
+++ app/src/main/java/com/example/petlover/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.petlover
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,9 +15,12 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-        signin_btn.setOnClickListener {
-            val intent = Intent(this, Bottomnavigation::class.java)
-            startActivity(intent)
-        }
+    }
+
+    fun loginfun(){
+        val email = findViewById<TextInputEditText>(R.id.inputEmaillogin)
+        val pass = findViewById<TextInputEditText>(R.id.passlogin)
+
+
     }
 }
--- app/src/main/java/com/example/petlover/RegisterActivity.kt
+++ app/src/main/java/com/example/petlover/RegisterActivity.kt
@@ -1,18 +1,70 @@
 package com.example.petlover
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.signup_btn
 
 class RegisterActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         signup_btn.setOnClickListener {
-            val intent = Intent(this, Bottomnavigation::class.java)
-            startActivity(intent)
+            registerfun()
+            //val intent = Intent(this, Bottomnavigation::class.java)
+            //startActivity(intent)
+
         }
+
     }
+
+    private fun registerfun(){
+        val username = findViewById<TextInputEditText>(R.id.inputEmailreg).text.toString()
+        val pass = findViewById<TextInputEditText>(R.id.inputPassreg).text.toString()
+        val confirmpass = findViewById<TextInputEditText>(R.id.cPassreg).text.toString()
+        val showrong =  findViewById<TextView>(R.id.showwrog)
+
+        Log.d("RegisterActivity", username)
+        Log.d("RegisterActivity",pass)
+        Log.d("RegisterActivity",confirmpass)
+        val checkmail: Boolean = isEmailValid(username)
+        if (checkmail && confirmpass == pass && pass.length <= 6){
+            showrong.visibility = View.GONE
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(username,pass)
+                .addOnCompleteListener{
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    Log.d("RegisterActivity","Succefuly ${it.result?.user?.uid}")
+                }
+        }
+        else{
+            var somwrong: String= ""
+            if (pass.length != 6){
+                somwrong += "Your password need least than 6 character\n"
+            }
+            if (confirmpass != pass){
+                somwrong += "Your password and confirmation password do not match.\n"
+            }
+            if (!checkmail){
+                somwrong += "Wrong Pattern in E-mail\n"
+            }
+            if (username.isEmpty()){
+                somwrong += "Enter Your E-mail\n"
+            }
+            if (pass.isEmpty()){
+                somwrong += "Enter Your Password\n"
+            }
+            showrong.text = somwrong
+            showrong.visibility = View.VISIBLE
+        }
+
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
 }
