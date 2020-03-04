c115e77aeeb77b2094b0af8b433cfc8489bb5696
ThanawatPermngram <thanawatpar@kkumail.com>
2020-03-04 12:21:44 +0700

    login fun half completed

--- app/src/main/java/com/example/petlover/LoginActivity.kt
+++ app/src/main/java/com/example/petlover/LoginActivity.kt
@@ -3,14 +3,19 @@ package com.example.petlover
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signin_btn.setOnClickListener {
+            loginfun()
+        }
         signup_btn.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
@@ -18,8 +23,19 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginfun(){
-        val email = findViewById<TextInputEditText>(R.id.inputEmaillogin)
-        val pass = findViewById<TextInputEditText>(R.id.passlogin)
+        val email = findViewById<TextInputEditText>(R.id.inputEmaillogin).text.toString()
+        val pass = findViewById<TextInputEditText>(R.id.passlogin).text.toString()
+        Log.d("login",email)
+        Log.d("login",pass)
+        auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email,pass)
+            .addOnCompleteListener{
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("login","Successfully login ${it.result?.user?.uid}")
+
+            }
+
 
 
     }
